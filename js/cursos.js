f7b9c650296ba35b16c4f45c875c59a3718b93d0
jpalma76 <juan.palma.alvarez@gmail.com>
2024-04-13 22:08:03 -0400

    optimizacion de imagenes de certificados

--- js/cursos.js
+++ js/cursos.js
@@ -4,144 +4,138 @@ let cursos = [
     {
         "id": 1,
         "name": "Curso Programación Basica",
-        "image": "./img-certificados/curso_programacion_basica.png",
-        "url": "./img-certificados/curso_programacion_basica.png"
+        "image": "./img-certificados/curso_programacion_basica-min.png",
+        "url": "./img-certificados/curso_programacion_basica-min.png"
     },
     {
         "id": 2,
         "name": "Curso Básico de C#, Fundamentos Variables y Operadores",
-        "image": "./img-certificados/curso_basico_de_c_charp_fundamentos_variables_y_operadores.png",
-        "url": "./img-certificados/curso_basico_de_c_charp_fundamentos_variables_y_operadores.png"
+        "image": "./img-certificados/curso_basico_de_c_charp_fundamentos_variables_y_operadores-min.png",
+        "url": "./img-certificados/curso_basico_de_c_charp_fundamentos_variables_y_operadores-min.png"
     },
     {
         "id": 3,
         "name": "Curso Básico de JavaScript",
-        "image": "./img-certificados/curso_basico_de_javascript.png",
-        "url": "./img-certificados/curso_basico_de_javascript.png"
+        "image": "./img-certificados/curso_basico_de_javascript-min.png",
+        "url": "./img-certificados/curso_basico_de_javascript-min.png"
     },
 
     {
         "id": 4,
         "name": "curso de frontend developer",
-        "image": "./img-certificados/curso_frontend_developer.png",
-        "url": "./img-certificados/curso_frontend_developer.png"
+        "image": "./img-certificados/curso_frontend_developer-min.png",
+        "url": "./img-certificados/curso_frontend_developer-min.png"
     },
     {
         "id": 5,
         "name": "curso practico de frontend developer",
-        "image": "./img-certificados/curso_frontend_developer_practico.png",
-        "url": "./img-certificados/curso_frontend_developer_practico.png"
+        "image": "./img-certificados/curso_frontend_developer_practico-min.png",
+        "url": "./img-certificados/curso_frontend_developer_practico-min.png"
     },
     {
         "id": 6,
         "name": "curso html y css practico",
-        "image": "./img-certificados/curso_html_css_practico.png",
-        "url": "./img-certificados/curso_html_css_practico.png"
+        "image": "./img-certificados/curso_html_css_practico-min.png",
+        "url": "./img-certificados/curso_html_css_practico-min.png"
 
     },
     {
         "id": 7,
         "name": "curso de fundamentos de punto net",
-        "image": "./img-certificados/Curso_fundamentos_de_punto_net.png",
-        "url": "./img-certificados/Curso_fundamentos_de_punto_net.png"
+        "image": "./img-certificados/curso_fundamentos_de_punto_net-min.png",
+        "url": "./img-certificados/curso_fundamentos_de_punto_net-min.png"
     },
     {
         "id": 8,
         "name": "curso definitivo html y css",
-        "image": "./img-certificados/curso_html_css_definitivo.png",
-        "url": "./img-certificados/curso_html_css_definitivo.png"
-    },
-    {
-        "id": 9,
-        "name": "curso de introduccion a platzi",
-        "image": "./img-certificados/curso_introduccion_a_platzi.png",
-        "url": "./img-certificados/curso_introduccion_a_platzi.png"
+        "image": "./img-certificados/curso_html_css_definitivo-min.png",
+        "url": "./img-certificados/curso_html_css_definitivo-min.png"
     },
 
     {
-        "id": 10,
+        "id": 9,
         "name": "curso introduccion al desarrollo backend",
-        "image": "./img-certificados/curso_introduccion_al_desarrollo_backend.png",
-        "url": "./img-certificados/curso_introduccion_al_desarrollo_backend.png"
+        "image": "./img-certificados/curso_introduccion_al_desarrollo_backend-min.png",
+        "url": "./img-certificados/curso_introduccion_al_desarrollo_backend-min.png"
+    },
+    {
+        "id": 10,
+        "name": "curso npm gestion de paquetes y dependencias en javascript",
+        "image": "./img-certificados/curso_npm_gestion_de_paquetes_y_dependencias_en_javascript-min.png",
+        "url": "./img-certificados/curso_npm_gestion_de_paquetes_y_dependencias_en_javascript-min.png"
     },
     {
         "id": 11,
-        "name": "curso npm gestion de paquetes y dependencias en javascript",
-        "image": "./img-certificados/curso_npm_gestion_de_paquetes_y_dependencias_en_javascript.png",
-        "url": "./img-certificados/curso_npm_gestion_de_paquetes_y_dependencias_en_javascript.png"
+        "name": "curso pensamiento logico de lenguajes de programación",
+        "image": "./img-certificados/curso_pensamiento_logico_lenguajes_de_programacion-min.png",
+        "url": "./img-certificados/curso_pensamiento_logico_lenguajes_de_programacion-min.png"
     },
     {
         "id": 12,
-        "name": "curso pensamiento logico de lenguajes de programación",
-        "image": "./img-certificados/curso_pensamiento_logico_lenguajes_de_programacion.png",
-        "url": "./img-certificados/curso_pensamiento_logico_lenguajes_de_programacion.png"
+        "name": "curso de php instalacion fundamentos y operaciones basico",
+        "image": "./img-certificados/curso_php_instalacion_fundamentos_y_operaciones_basico-min.png",
+        "url": "./img-certificados/curso_php_instalacion_fundamentos_y_operaciones_basico-min.png"
     },
     {
         "id": 13,
-        "name": "curso de php instalacion fundamentos y operaciones basico",
-        "image": "./img-certificados/curso_php_instalacion_fundamentos_y_operaciones_basico.png",
-        "url": "./img-certificados/curso_php_instalacion_fundamentos_y_operaciones_basico.png"
+        "name": "curso practico de php",
+        "image": "./img-certificados/curso_practico_php-min.png",
+        "url": "./img-certificados/curso_practico_php-min.png"
     },
     {
         "id": 14,
-        "name": "curso practico de php",
-        "image": "./img-certificados/curso_practico_php.png",
-        "url": "./img-certificados/curso_practico_php.png"
-    },
-    {
-        "id": 15,
         "name": "curso  integracion de php y html",
-        "image": "./img-certificados/curso_de_integracion_de_php_con_html.png",
-        "url": "./img-certificados/curso_de_integracion_de_php_con_html.png"
+        "image": "./img-certificados/curso_de_integracion_de_php_con_html-min.png",
+        "url": "./img-certificados/curso_de_integracion_de_php_con_html-min.png"
     },
 
     {
-        "id": 16,
+        "id": 15,
         "name": "curso prework linux: Configuracion de entorno de desarrollo en linux",
-        "image": "./img-certificados/curso_prework_linux.png",
-        "url": "./img-certificados/curso_prework_linux.png"
+        "image": "./img-certificados/curso_prework_linux-min.png",
+        "url": "./img-certificados/curso_prework_linux-min.png"
+    },
+    {
+        "id": 16,
+        "name": "curso prework: configuracion de entorno de desarrollo en windows",
+        "image": "./img-certificados/curso_prework_windows-min.png",
+        "url": "./img-certificados/curso_prework_windows-min.png"
     },
     {
         "id": 17,
-        "name": "curso prework: configuracion de entorno de desarrollo en windows",
-        "image": "./img-certificados/curso_prework_windows.png",
-        "url": "./img-certificados/curso_prework_windows.png"
+        "name": "curso javascript nivel 1",
+        "image": "./img-certificados/curso_javascript_nivel1-min.png",
+        "url": "./img-certificados/curso_javascript_nivel1-min.png"
     },
     {
         "id": 18,
-        "name": "curso javascript nivel 1",
-        "image": "./img-certificados/curso_javascript_nivel1.png",
-        "url": "./img-certificados/curso_javascript_nivel1.png"
+        "name": "curso javascript nivel 2",
+        "image": "./img-certificados/curso_javascript_nivel2-min.png",
+        "url": "./img-certificados/curso_javascript_nivel2-min.png"
     },
     {
-        "id": 19,
-        "name": "curso javascript nivel 2",
-        "image": "./img-certificados/curso_javascript_nivel2.png",
-        "url": "./img-certificados/curso_javascript_nivel2.png"
+        "id": 29,
+        "name": "Curso PHP nivel 1 - Jóvenes Programadores",
+        "image": "./img-certificados/curso_php_jovenes_programadores-min.png",
+        "url": "./img-certificados/curso_php_jovenes_programadores-min.png"
     },
     {
         "id": 20,
-        "name": "Curso PHP nivel 1 - Jóvenes Programadores",
-        "image": "./img-certificados/curso_php_jovenes_programadores.png",
-        "url": "./img-certificados/curso_php_jovenes_programadores.png"
+        "name": "Curso CSS - Jóvenes Programadores",
+        "image": "./img-certificados/curso_css_jovenesprogramadores-min.png",
+        "url": "./img-certificados/curso_css_jovenesprogramadores-min.png"
     },
     {
         "id": 21,
-        "name": "Curso CSS - Jóvenes Programadores",
-        "image": "./img-certificados/curso_css_jovenesprogramadores.png",
-        "url": "./img-certificados/curso_css_jovenesprogramadores.png"
+        "name": "Curso Introducción a Python - Jóvenes Programadores",
+        "image": "./img-certificados/curso-introduccion-a-python-min.png",
+        "url": "./img-certificados/curso-introduccion-a-python-min.png"
     },
     {
         "id": 22,
-        "name": "Curso Introducción a Python - Jóvenes Programadores",
-        "image": "./img-certificados/curso-introduccion-a-python.png",
-        "url": "./img-certificados/curso-introduccion-a-python.png"
-    },
-    {
-        "id": 23,
         "name": "Curso de React - Jóvenes Programadores",
-        "image": "./img-certificados/curso-de-react.png",
-        "url": "./img-certificados/curso-de-react.png"
+        "image": "./img-certificados/curso-de-react-min.png",
+        "url": "./img-certificados/curso-de-react-min.png"
     }
 ];
 
@@ -152,7 +146,10 @@ cursos.forEach(curso => {
     <div class="grid-item">
         <div className="card-item">
             <a href="${curso.url}" target="_blank" rel="noopener noreferrer">
-            <img class="img-cert" src="${curso.image}" alt="${curso.name}">
+                <picture>
+                    <img class="img-cert" src="${curso.image}" alt="${curso.name}">
+                </picture>
+
                 <div class="nombre-curso">
                     <p class="img-name">${curso.name}</p>
                 </div>
--- js/skills.js
+++ js/skills.js
@@ -4,44 +4,44 @@ let cursos = [
     {
         "id": 1,
         "name": "Aplicación de Herramientas para la Certificación de Calidad de Servicios de Telecomunicaciones HFC",
-        "image": "./img-certificados/certificaciones-vtr/aplicación_de_herramientas_para_la_certificación_de_calidad_de_servicios_de_telecomunicaciones.png",
-        "url": "./img-certificados/certificaciones-vtr/aplicación_de_herramientas_para_la_certificación_de_calidad_de_servicios_de_telecomunicaciones.png"
+        "image": "./img-certificados/certificaciones-vtr/aplicación_de_herramientas_para_la_certificación_de_calidad_de_servicios_de_telecomunicaciones-min.png",
+        "url": "./img-certificados/certificaciones-vtr/aplicación_de_herramientas_para_la_certificación_de_calidad_de_servicios_de_telecomunicaciones-min.png"
     },
     {
         "id": 2,
        "name": "Aplicación Práctica de Procotolos de Atención y Servicios para la Atención de Clientes",
-        "image": "./img-certificados/certificaciones-vtr/aplicacion_practica_de_procotolos_de_atencion_y_servicios_para_la_atencion_de_clientes.png",
-        "url": "./img-certificados/certificaciones-vtr/aplicacion_practica_de_procotolos_de_atencion_y_servicios_para_la_atencion_de_clientes.png"
+        "image": "./img-certificados/certificaciones-vtr/aplicacion_practica_de_procotolos_de_atencion_y_servicios_para_la_atencion_de_clientes-min.png",
+        "url": "./img-certificados/certificaciones-vtr/aplicacion_practica_de_procotolos_de_atencion_y_servicios_para_la_atencion_de_clientes-min.png"
     },
     {
         "id": 3,
        "name": "instalación de los Servicios en Redes HFC (Hibrido de Fibra Coaxial)",
-        "image": "./img-certificados/certificaciones-vtr/instalacion_de_los_servicios_en_redes_hfc_(hibrido_de_fibra_coaxial).png",
-        "url": "./img-certificados/certificaciones-vtr/instalacion_de_los_servicios_en_redes_hfc_(hibrido_de_fibra_coaxial).png"
+        "image": "./img-certificados/certificaciones-vtr/instalacion_de_los_servicios_en_redes_hfc_(hibrido_de_fibra_coaxial)-min.png",
+        "url": "./img-certificados/certificaciones-vtr/instalacion_de_los_servicios_en_redes_hfc_(hibrido_de_fibra_coaxial)-min.png"
     },
     {
         "id": 4,
        "name": "Instalación de productos domiciliarios",
-        "image": "./img-certificados/certificaciones-vtr/instalacion_de_productos_domiciliarios.png",
-        "url": "./img-certificados/certificaciones-vtr/instalacion_de_productos_domiciliarios.png"
+        "image": "./img-certificados/certificaciones-vtr/instalacion_de_productos_domiciliarios-min.png",
+        "url": "./img-certificados/certificaciones-vtr/instalacion_de_productos_domiciliarios-min.png"
     },
     {
         "id": 5,
        "name": "Instalación de Servicios en redes FTTH",
-        "image": "./img-certificados/certificaciones-vtr/instalacion_de_servicios_en_redes_ftth.png",
-        "url": "./img-certificados/certificaciones-vtr/instalacion_de_servicios_en_redes_ftth.png"
+        "image": "./img-certificados/certificaciones-vtr/instalacion_de_servicios_en_redes_ftth-min.png",
+        "url": "./img-certificados/certificaciones-vtr/instalacion_de_servicios_en_redes_ftth-min.png"
     },
     {
         "id": 6,
        "name": "Manejo de Procedimiento de Instalaciones de Calidad",
-        "image": "./img-certificados/certificaciones-vtr/manejo_de_procedimiento_de_instalaciones_de_calidad.png",
-        "url": "./img-certificados/certificaciones-vtr/manejo_de_procedimiento_de_instalaciones_de_calidad.png"
+        "image": "./img-certificados/certificaciones-vtr/manejo_de_procedimiento_de_instalaciones_de_calidad-min.png",
+        "url": "./img-certificados/certificaciones-vtr/manejo_de_procedimiento_de_instalaciones_de_calidad-min.png"
     },
     {
         "id": 7,
        "name": "Trabajo en Altura para la Industria de Telecomunicaciones",
-        "image": "./img-certificados/certificaciones-vtr/trabajo_en_altura_para_la_industria_de_telecomunicaciones.png",
-        "url": "./img-certificados/certificaciones-vtr/trabajo_en_altura_para_la_industria_de_telecomunicaciones.png"
+        "image": "./img-certificados/certificaciones-vtr/trabajo_en_altura_para_la_industria_de_telecomunicaciones-min.png",
+        "url": "./img-certificados/certificaciones-vtr/trabajo_en_altura_para_la_industria_de_telecomunicaciones-min.png"
     }
 ];
 let card = document.getElementById('skill-container');
@@ -51,10 +51,12 @@ cursos.forEach(curso => {
     <div class="grid-item">
         <div className="card-item">
             <a href="${curso.url}"  target="_blank" rel="noopener noreferrer">
-            <img class="img-cert" src="${curso.image}" alt="${curso.name}">
-                <div class="nombre-curso">
-                    <p class="img-name">${curso.name}</p>
-                </div>
+            <picture>
+                <img class="img-cert" src="${curso.image}" alt="${curso.name}">
+            </picture>
+            <div class="nombre-curso">
+                <p class="img-name">${curso.name}</p>
+            </div>
             </a>
         </div>
 
