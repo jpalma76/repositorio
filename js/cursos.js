db452e893796dba6ef2bb9e856ae8b0862351988
Juan Palma <juan.palma.alvarez@gmail.com>
2024-08-31 19:17:25 -0400

    Agrega certificados de cursos achs

--- js/cursos.js
+++ js/cursos.js
@@ -4,138 +4,138 @@ let cursos = [
     {
         "id": 1,
         "name": "Curso Programación Basica",
-        "image": "./img-certificados/curso_programacion_basica-min.png",
-        "url": "./img-certificados/curso_programacion_basica-min.png"
+        "image": "./img-certificados/platzi/curso_de_programacion_basica.png",
+        "url": "./img-certificados/platzi/curso_de_programacion_basica.png"
     },
     {
         "id": 2,
         "name": "Curso Básico de C#, Fundamentos Variables y Operadores",
-        "image": "./img-certificados/curso_basico_de_c_charp_fundamentos_variables_y_operadores-min.png",
-        "url": "./img-certificados/curso_basico_de_c_charp_fundamentos_variables_y_operadores-min.png"
+        "image": "./img-certificados/platzi/curso_basico_de_c_sharp_fundamentos_variables_y_operadores.png",
+        "url": "./img-certificados/platzi/curso_basico_de_c_sharp_fundamentos_variables_y_operadores.png"
     },
     {
         "id": 3,
         "name": "Curso Básico de JavaScript",
-        "image": "./img-certificados/curso_basico_de_javascript-min.png",
-        "url": "./img-certificados/curso_basico_de_javascript-min.png"
+        "image": "./img-certificados/platzi/curso_basico_de_javascript.png",
+        "url": "./img-certificados/platzi/curso_basico_de_javascript.png"
     },
 
     {
         "id": 4,
         "name": "curso de frontend developer",
-        "image": "./img-certificados/curso_frontend_developer-min.png",
-        "url": "./img-certificados/curso_frontend_developer-min.png"
+        "image": "./img-certificados/platzi/curso_de_frontend_developer.png",
+        "url": "./img-certificados/platzi/curso_de_frontend_developer.png"
     },
     {
         "id": 5,
         "name": "curso practico de frontend developer",
-        "image": "./img-certificados/curso_frontend_developer_practico-min.png",
-        "url": "./img-certificados/curso_frontend_developer_practico-min.png"
+        "image": "./img-certificados/platzi/curso_practico_de_frontend_developer.png",
+        "url": "./img-certificados/platzi/curso_practico_de_frontend_developer.png"
     },
     {
         "id": 6,
         "name": "curso html y css practico",
-        "image": "./img-certificados/curso_html_css_practico-min.png",
-        "url": "./img-certificados/curso_html_css_practico-min.png"
+        "image": "./img-certificados/platzi/curso_practico_html_css.png",
+        "url": "./img-certificados/platzi/curso_practico_html_css.png"
 
     },
     {
         "id": 7,
         "name": "curso de fundamentos de punto net",
-        "image": "./img-certificados/curso_fundamentos_de_punto_net-min.png",
-        "url": "./img-certificados/curso_fundamentos_de_punto_net-min.png"
+        "image": "./img-certificados/platzi/Curso_de_punto_net.png",
+        "url": "./img-certificados/platzi/Curso_de_punto_net.png"
     },
     {
         "id": 8,
         "name": "curso definitivo html y css",
-        "image": "./img-certificados/curso_html_css_definitivo-min.png",
-        "url": "./img-certificados/curso_html_css_definitivo-min.png"
+        "image": "./img-certificados/platzi/curso_html_css_definitivo-min.png",
+        "url": "./img-certificados/platzi/curso_html_css_definitivo-min.png"
     },
 
     {
         "id": 9,
         "name": "curso introduccion al desarrollo backend",
-        "image": "./img-certificados/curso_introduccion_al_desarrollo_backend-min.png",
-        "url": "./img-certificados/curso_introduccion_al_desarrollo_backend-min.png"
+        "image": "./img-certificados/platzi/curso_de_introduccion_al_desarrollo_backend.png",
+        "url": "./img-certificados/platzi/curso_de_introduccion_al_desarrollo_backend.png"
     },
     {
         "id": 10,
         "name": "curso npm gestion de paquetes y dependencias en javascript",
-        "image": "./img-certificados/curso_npm_gestion_de_paquetes_y_dependencias_en_javascript-min.png",
-        "url": "./img-certificados/curso_npm_gestion_de_paquetes_y_dependencias_en_javascript-min.png"
+        "image": "./img-certificados/platzi/curso_npm_gestion_de_paquetes_y_dependencias_en_javascript.png",
+        "url": "./img-certificados/platzi/curso_npm_gestion_de_paquetes_y_dependencias_en_javascript.png"
     },
     {
         "id": 11,
         "name": "curso pensamiento logico de lenguajes de programación",
-        "image": "./img-certificados/curso_pensamiento_logico_lenguajes_de_programacion-min.png",
-        "url": "./img-certificados/curso_pensamiento_logico_lenguajes_de_programacion-min.png"
+        "image": "./img-certificados/platzi/curso_pensamiento_logico_lenguajes_de_programacion.png",
+        "url": "./img-certificados/platzi/curso_pensamiento_logico_lenguajes_de_programacion.png"
     },
     {
         "id": 12,
         "name": "curso de php instalacion fundamentos y operaciones basico",
-        "image": "./img-certificados/curso_php_instalacion_fundamentos_y_operaciones_basico-min.png",
-        "url": "./img-certificados/curso_php_instalacion_fundamentos_y_operaciones_basico-min.png"
+        "image": "./img-certificados/platzi/curso_php_instalacion_fundamentos_y_operaciones_basico.png",
+        "url": "./img-certificados/platzi/curso_php_instalacion_fundamentos_y_operaciones_basico.png"
     },
     {
         "id": 13,
         "name": "curso practico de php",
-        "image": "./img-certificados/curso_practico_php-min.png",
-        "url": "./img-certificados/curso_practico_php-min.png"
+        "image": "./img-certificados/platzi/curso_practico_php.png",
+        "url": "./img-certificados/platzi/curso_practico_php.png"
     },
     {
         "id": 14,
         "name": "curso  integracion de php y html",
-        "image": "./img-certificados/curso_de_integracion_de_php_con_html-min.png",
-        "url": "./img-certificados/curso_de_integracion_de_php_con_html-min.png"
+        "image": "./img-certificados/platzi/curso_practico_php.png",
+        "url": "./img-certificados/platzi/curso_practico_php.png"
     },
 
     {
         "id": 15,
         "name": "curso prework linux: Configuracion de entorno de desarrollo en linux",
-        "image": "./img-certificados/curso_prework_linux-min.png",
-        "url": "./img-certificados/curso_prework_linux-min.png"
+        "image": "./img-certificados/platzi/curso_de_prework_configuracion_de_entorno_de_desarrollo_en_linux.png",
+        "url": "./img-certificados/platzi/curso_de_prework_configuracion_de_entorno_de_desarrollo_en_linux.png"
     },
     {
         "id": 16,
         "name": "curso prework: configuracion de entorno de desarrollo en windows",
-        "image": "./img-certificados/curso_prework_windows-min.png",
-        "url": "./img-certificados/curso_prework_windows-min.png"
+        "image": "./img-certificados/platzi/curso_de_prework_configuracion_de_entorno_de_desarrollo_en_windows.png",
+        "url": "./img-certificados/platzi/curso_de_prework_configuracion_de_entorno_de_desarrollo_en_windows.png"
     },
     {
         "id": 17,
-        "name": "curso javascript nivel 1",
-        "image": "./img-certificados/curso_javascript_nivel1-min.png",
-        "url": "./img-certificados/curso_javascript_nivel1-min.png"
+        "name": "curso javascript nivel 1 - Jóvenes Programadores",
+        "image": "./img-certificados/jovenes-programadores/curso_javascript_nivel1-min.png",
+        "url": "./img-certificados/jovenes-programadores/curso_javascript_nivel1-min.png"
     },
     {
         "id": 18,
-        "name": "curso javascript nivel 2",
-        "image": "./img-certificados/curso_javascript_nivel2-min.png",
-        "url": "./img-certificados/curso_javascript_nivel2-min.png"
+        "name": "curso javascript nivel 2 - Jóvenes Programadores",
+        "image": "./img-certificados/jovenes-programadores/curso_javascript_nivel2-min.png",
+        "url": "./img-certificados/jovenes-programadores/curso_javascript_nivel2-min.png"
     },
     {
         "id": 29,
         "name": "Curso PHP nivel 1 - Jóvenes Programadores",
-        "image": "./img-certificados/curso_php_jovenes_programadores-min.png",
-        "url": "./img-certificados/curso_php_jovenes_programadores-min.png"
+        "image": "./img-certificados/jovenes-programadores/curso_php_jovenes_programadores-min.png",
+        "url": "./img-certificados/jovenes-programadores/curso_php_jovenes_programadores-min.png"
     },
     {
         "id": 20,
         "name": "Curso CSS - Jóvenes Programadores",
-        "image": "./img-certificados/curso_css_jovenesprogramadores-min.png",
-        "url": "./img-certificados/curso_css_jovenesprogramadores-min.png"
+        "image": "./img-certificados/jovenes-programadores/curso_css_jovenesprogramadores-min.png",
+        "url": "./img-certificados/jovenes-programadores/curso_css_jovenesprogramadores-min.png"
     },
     {
         "id": 21,
         "name": "Curso Introducción a Python - Jóvenes Programadores",
-        "image": "./img-certificados/curso-introduccion-a-python-min.png",
-        "url": "./img-certificados/curso-introduccion-a-python-min.png"
+        "image": "./img-certificados/jovenes-programadores/curso-introduccion-a-python-min.png",
+        "url": "./img-certificados/jovenes-programadores/curso-introduccion-a-python-min.png"
     },
     {
         "id": 22,
         "name": "Curso de React - Jóvenes Programadores",
-        "image": "./img-certificados/curso-de-react-min.png",
-        "url": "./img-certificados/curso-de-react-min.png"
+        "image": "./img-certificados/jovenes-programadores/curso-de-react-min.png",
+        "url": "./img-certificados/jovenes-programadores/curso-de-react-min.png"
     }
 ];
 
--- js/skills.js
+++ js/skills.js
@@ -40,8 +40,69 @@ let cursos = [
     {
         "id": 7,
        "name": "Trabajo en Altura para la Industria de Telecomunicaciones",
-        "image": "./img-certificados/certificaciones-vtr/trabajo_en_altura_para_la_industria_de_telecomunicaciones-min.png",
-        "url": "./img-certificados/certificaciones-vtr/trabajo_en_altura_para_la_industria_de_telecomunicaciones-min.png"
+        "image": "./img-certificados/cursos-achs/Seguridad_electrica_general_N1.png",
+        "url": "./img-certificados/cursos-achs/Seguridad_electrica_general_N1.png"
+    }
+    ,
+    {
+        "id": 8,
+       "name": "Seguridad Eléctrica General Nº 1",
+        "image": "./img-certificados/cursos-achs/Seguridad_electrica_general_N1.png",
+        "url": "./img-certificados/cursos-achs/Seguridad_electrica_general_N1.png"
+    }
+    ,
+    {
+        "id": 9,
+        "name": "Seguridad Eléctrica Operacional Nº 2",
+         "image": "./img-certificados/cursos-achs/seguridad_electrica_operacional_N2.png",
+         "url": "./img-certificados/cursos-achs/seguridad_electrica_operacional_N2.png"
+    }
+    ,
+    {
+        "id": 10,
+       "name": "Identificación de Peligros y Evaluación de Riesgos en el Trabajo",
+        "image": "./img-certificados/cursos-achs/identificacion_de_peligros_y_evaluacion_de_riesgos_en_el_trabajo.png",
+        "url": "./img-certificados/cursos-achs/identificacion_de_peligros_y_evaluacion_de_riesgos_en_el_trabajo.png"
+    }
+    ,
+    {
+        "id": 11,
+       "name": "Conducción Defensiva en Vehículos Livianos",
+        "image": "./img-certificados/cursos-achs/conduccion_defensiva_en_vehiculos_livianos.png",
+        "url": "./img-certificados/cursos-achs/conduccion_defensiva_en_vehiculos_livianos.png"
+    }
+    ,
+    {
+        "id": 12,
+       "name": "12.- Medidas Preventivas Frente a Radiación UV",
+        "image": "./img-certificados/cursos-achs/medidas_preventivas_frente_a_radiacion_uv.png",
+        "url": "./img-certificados/cursos-achs/medidas_preventivas_frente_a_radiacion_uv.png"
+    },
+    {
+        "id": 13,
+       "name": "13.- Uso de Extintores",
+        "image": "./img-certificados/cursos-achs/uso de extintores.png",
+        "url": "./img-certificados/cursos-achs/uso de extintores.png"
+    }
+    ,
+    {
+        "id": 14,
+       "name": "14.- Primera Respuesta Frente a Emergencias de Salud",
+        "image": "./img-certificados/cursos-achs/primera_respuesta_frente_a_emergencias_de_salud.png",
+        "url": "./img-certificados/cursos-achs/primera_respuesta_frente_a_emergencias_de_salud.png"
+    },
+    {
+        "id": 15,
+       "name": "15.- Manejo Manual de Cargas",
+        "image": "./img-certificados/cursos-achs/manejo_manual_de_cargas.png",
+        "url": "./img-certificados/cursos-achs/manejo_manual_de_cargas.png"
+    }
+    ,
+    {
+        "id": 16,
+       "name": "16.- Evitando Dolencias Músculo esqueléticas de Extremidades Superiores (TMERT) en su Trabajo",
+        "image": "./img-certificados/cursos-achs/evitando_dolencias_musculoesqueleticas_de_extremidades_superiores-TMERT_en_su_trabajo.png",
+        "url": "./img-certificados/cursos-achs/evitando_dolencias_musculoesqueleticas_de_extremidades_superiores-TMERT_en_su_trabajo.png"
     }
 ];
 let card = document.getElementById('skill-container');
